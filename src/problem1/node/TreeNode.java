ecd41ac6c2057b826844badfd43cefe6495ae819
Krishna kant sharma <krishna.sharma_cs18@gla.ac.in>
2020-03-26 23:47:40 +0530

    TreeNode class created

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -1,10 +1,45 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
+ *  User: krishna kant sharma
+ *  Date: 26-Mar-20
  *  Time: 7:17 PM
  */
 package problem1.node;
 
 public class TreeNode {
+    //variabales to store data and refrences
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+
+    //getter and setter method to access and initialise private variables
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
 }
