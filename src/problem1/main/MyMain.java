7ca6cd470de542c33e3611decd3769dabff225af
Krishna kant sharma <krishna.sharma_cs18@gla.ac.in>
2020-03-26 23:51:12 +0530

    main class MyBinarySearchTree created question 1 complete

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,11 +1,34 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
+ *  User: krishna kant sharma
+ *  Date: 26-Mar-20
+ *  Time: 11:47 PM
  */
 package problem1.main;
 // executable class
 
-public class MyMain {
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+
+public abstract class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree mst = new MyBinarySearchTree();
+        TreeNode root;
+        root = new TreeNode(10);
+        mst.insert(root, 5);
+        mst.insert(root, 1);
+        mst.insert(root, 15);
+        mst.insert(root, 9);
+        mst.insert(root, 7);
+        mst.insert(root, 12);
+        mst.insert(root, 30);
+        mst.insert(root, 25);
+        mst.insert(root, 40);
+        mst.insert(root, 45);
+        mst.insert(root, 6);
+        //mst.inorderRec(root);
+        //mst.leftViewUtil(root,1);
+        //mst.countNodeWithOutLeftChild(root);
+        mst.leftView(root);
+    }
 }
