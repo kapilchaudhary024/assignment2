cfd6cef3fbc4636d8edd03a263e7b717782f0c49
Krishna kant sharma <krishna.sharma_cs18@gla.ac.in>
2020-03-27 18:28:15 +0530

    question 5 completed Assignment completed

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -1,10 +1,32 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
 package problem5.node;
+
+import problem5.student.Student;
+
 // to define node properties
 public class Node {
+    private Student s;
+    private Node next;
+
+
+    public Node(Student s) {
+        this.s = s;
+        next = null;
+    }
+
+    public Student getS() {
+        return s;
+    }
+
+    public void setS(Student s) {
+        this.s = s;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
 }
